ae356d0fba92e2807f746d61151828de636f556b
Nipuna Adikaram <nipuna775@gmail.com>
2022-11-14 18:32:51 +1100

    adding java script

---
+++ email.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('button')
+
+const inputs = document.querySelector('form')
+
+btn.addEventListener('click',() =>{
+Email.send({
+    Host:"smtp.mailtrap.io",
+    Username:"c32e131550aecb",
+    Password: "6786a2c573a9c5",
+    To : "nipuna775@gmail.com",
+    From : inputs.elements["emial"].value,
+    Subject:" Contact us ",
+    Body:inputs.elements["any-questions"].value + "<br>" + inputs.elements["name"].value
+}) .then(msg=>alert(" The email succesfully sent"))
+})
